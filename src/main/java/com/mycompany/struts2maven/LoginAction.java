1edd948ce8a175883bfea9f4cce3e6c4296b72d3
Anas <Anas@APL-Asus.home>
2014-12-17 22:27:57 -0500

    test

---
+++ src/main/java/com/mycompany/struts2maven/LoginAction.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.struts2maven;
+
+/**
+ *
+ * @author Anas
+ */
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.ResultPath;
+ 
+import com.opensymphony.xwork2.ActionSupport;
+ 
+@Namespace("/User")
+@ResultPath(value="/")
+@Result(name="success",location="pages/login.jsp")
+public class LoginAction extends ActionSupport{
+ 
+}
